443be7f85f5c2b4823c8feafd75170bed30c4c5a
Jason Domizio <jason.domizio@advent.com>
2018-08-10 09:11:48 -0400

    Add some resources :)

---
+++ src/pages/cotb2018.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Cotb2018 = () => (
+  <div>
+    <h1>Code on the Beach 2018</h1>
+    <div>
+        <div><a href="https://docs.google.com/presentation/d/1dhbL29CHw__viP52UnmMXvMFWqZNpxa_FYyrmWmecWg/edit?usp=sharing">Google Slides</a></div>
+        <div><a href="https://github.com/jdomizio/cotb2018">Code Examples</a></div>
+        <div><a href="https://github.com/jdomizio/guess.domiz.io">guess.domiz.io SignalR app</a></div>
+        <div><a href="http://guess.domiz.io">Link to guessing game (guess.domiz.io)</a></div>
+    </div>
+  </div>
+)
+
+export default Cotb2018
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome</h1>
-    <p>I think you probably took a wrong turn.</p>
+    <header>
+      <h1>Hi, I'm Jason</h1>
+      <p>I am a developer, and I didn't give my self enough time to make this page pretty ;)</p>
+    </header>
+    <div>
+      <Link to="/cotb2018">Code on the Beach 2018</Link>
+    </div>
   </div>
 )
 
